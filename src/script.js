6431ffb0e6ea2af541e0ededf975f63f6f8341c2
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2022-05-18 15:17:28 +0100

    Transferred JavaScript

---
+++ src/script.js
@@ -0,0 +1,78 @@
+function showDateAndTime(currentDateAndTime) {
+  let days = [
+    `Sunday`,
+    `Monday`,
+    `Tuesday`,
+    `Wednesday`,
+    `Thursday`,
+    `Friday`,
+    `Saturday`,
+  ];
+
+  let day = days[currentDateAndTime.getDay()];
+  let hours = currentDateAndTime.getHours();
+  let minutes = currentDateAndTime.getMinutes();
+  if (hours < 10) {
+    let hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    let minutes = `0${minutes}`;
+  }
+  let time = `${hours}:${minutes}`;
+  let DisplayDateAndTime = `${day} ${time}`;
+  return DisplayDateAndTime;
+}
+
+function displayWeather(response) {
+  document.querySelector(".city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].description;
+}
+
+function searchCity(city) {
+  let apiKey = `5c32603554bdbae2e57c8722d88e7625`;
+  let unit = `metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function cityFormSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#country-input").value;
+  searchCity(city);
+}
+function searchLocation(position) {
+  let apiKey = `5c32603554bdbae2e57c8722d88e7625`;
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let unit = `metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${unit}&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function findCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let dayTime = document.querySelector("#time-date");
+dayTime.innerHTML = showDateAndTime(new Date());
+
+let searchForm = document.querySelector("#search-country-form");
+searchForm.addEventListener("submit", cityFormSubmit);
+
+let nowWeatherButton = document.querySelector("#now-button");
+nowWeatherButton.addEventListener("click", findCurrentLocation);
+
+searchCity(`London`);
